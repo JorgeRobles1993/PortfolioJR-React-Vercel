8a2d22b833eb27f5a899ce4c68f8e4c8361cd36c
JorgeRobles1993 <jorgejoanrobles93@gmail.com>
2024-07-15 11:21:25 +0200

    added techs

--- src/MyWork.js
+++ src/MyWork.js
@@ -1,24 +1,27 @@
 import React from 'react';
 
 const projects = [
-  { id: 1, title: 'Project 1', imgSrc: 'images/farmpad.png', link: 'https://jorge-farmpad.projets.p8.garage404.com/' },
-  { id: 2, title: 'Project 2', imgSrc: 'images/ThePhiloQuizz.png', link: 'https://jorge-philo-quizzdef.projets.p8.garage404.com/' },
-  { id: 3, title: 'Project 3', imgSrc: 'images/Clonestagram.png', link: 'https://jorge-clonestagram.projets.p8.garage404.com/' },
-  { id: 4, title: 'Project 4', imgSrc: 'images/PokemonAutoFight.png', link: 'https://jorge-pokefight.projets.p8.garage404.com/' },
-  { id: 5, title: 'Project 5', imgSrc: 'images/LuxuryServices.png', link: 'https://jorge-p8-luxury-services.projets.p8.garage404.com/' },
-  { id: 6, title: 'Project 6', imgSrc: 'images/pcomposants.png', link: 'https://jorge-pcomposants.projets.p8.garage404.com/' },
-  { id: 7, title: 'Project 7', imgSrc: 'images/Morpion.png', link: 'https://jorge-morpion.projets.p8.garage404.com/' },
-  { id: 8, title: 'Project 8', imgSrc: 'images/MyWeatherApp.png', link: 'https://my-react-weather-app-five.vercel.app/' },
-  { id: 9, title: 'Project 9', imgSrc: 'images/SuperMarioRPG.png', link: 'https://random-rumble-react.vercel.app/' },
-  { id: 10, title: 'Project 10', imgSrc: 'images/AlpineCars.png', link: 'https://alpine-beta-murex.vercel.app/' },
+  { id: 1, title: 'The FarmPad', imgSrc: 'images/farmpad.png', link: 'https://jorge-farmpad.projets.p8.garage404.com/', technologies: ['HTML', 'CSS', 'JavaScript'] },
+  { id: 2, title: 'The Philo-Quiz', imgSrc: 'images/ThePhiloQuizz.png', link: 'https://jorge-philo-quizzdef.projets.p8.garage404.com/', technologies: ['HTML', 'CSS', 'PHP'] },
+  { id: 3, title: 'Clonestagram', imgSrc: 'images/Clonestagram.png', link: 'https://jorge-clonestagram.projets.p8.garage404.com/', technologies: ['HTML', 'CSS', 'PHP'] },
+  { id: 4, title: 'Pokemon AutoFight', imgSrc: 'images/PokemonAutoFight.png', link: 'https://jorge-pokefight.projets.p8.garage404.com/', technologies: ['HTML', 'CSS', 'Tailwind', 'JavaScript', 'PHP'] },
+  { id: 5, title: 'Luxury Services', imgSrc: 'images/LuxuryServices.png', link: 'https://jorge-p8-luxury-services.projets.p8.garage404.com/', technologies: ['Symfony', 'Twig', 'Materialize'] },
+  { id: 6, title: 'PComposants', imgSrc: 'images/pcomposants.png', link: 'https://jorge-pcomposants.projets.p8.garage404.com/', technologies: ['PrestaShop'] },
+  { id: 7, title: 'Morpion', imgSrc: 'images/Morpion.png', link: 'https://jorge-morpion.projets.p8.garage404.com/', technologies: ['HTML', 'CSS', 'React'] },
+  { id: 8, title: 'My Weather App', imgSrc: 'images/MyWeatherApp.png', link: 'https://my-react-weather-app-five.vercel.app/', technologies: ['React', 'API Open-Weather'] },
+  { id: 9, title: 'Super Mario RPG Battle Game', imgSrc: 'images/SuperMarioRPG.png', link: 'https://random-rumble-react.vercel.app/', technologies: ['React', 'Redux'] },
+  { id: 10, title: 'Alpine Cars', imgSrc: 'images/AlpineCars.png', link: 'https://alpine-beta-murex.vercel.app/', technologies: ['React', 'Redux'] },
 ];
 
 function MyWork() {
+  // Invertimos el orden de los proyectos utilizando slice().reverse().map()
+  const reversedProjects = projects.slice().reverse();
+
   return (
     <div className="my-work">
       <h2>My Work</h2>
       <div className="grid">
-        {projects.map((project) => (
+        {reversedProjects.map((project) => (
           <div key={project.id} className="project">
             <a href={project.link} target="_blank" rel="noopener noreferrer">
               <img src={project.imgSrc} alt={project.title} />
@@ -26,6 +29,10 @@ function MyWork() {
                 <h3>{project.title}</h3>
               </div>
             </a>
+            <div className="project-info">
+              <h3>{project.title}</h3>
+              <p>Technologies: {project.technologies.join(', ')}</p>
+            </div>
           </div>
         ))}
       </div>
